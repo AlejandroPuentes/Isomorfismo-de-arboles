7e83ff20e1bc48651db4e1d52b94bf5a46c5e30f
Orejas2001 <47760656+Orejas2001@users.noreply.github.com>
2020-08-03 23:25:12 -0500

    Mas o menos xd

--- src/arbolessimi/Arbol.java
+++ src/arbolessimi/Arbol.java
@@ -5,15 +5,22 @@
  */
 package arbolessimi;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+
 /**
  *
  * @author User
  */
 public class Arbol {
     Nodo raiz;
-    
+    ArrayList nombreA1;
+    ArrayList nombreA2;
     Arbol(){
         raiz = null;
+        nombreA1 = new ArrayList();
+        nombreA2 = new ArrayList();
     }
     
     public void insertar(int x, int dire){
@@ -41,10 +48,93 @@ public class Arbol {
                    }
                 }
             }           
+        }  
+    }
+  
+
+    public void Inorden(Nodo a1){
+        if(a1!=null){
+            Inorden(a1.Izq);
+            nombreA1.add(a1.Info);
+            Inorden(a1.Dere);
         }
-        
+    }
+  
+    public void Inorden2(Nodo a2){
+        if(a2!=null){
+            Inorden(a2.Izq);
+            nombreA2.add(a2.Info);
+            Inorden(a2.Dere);     
+        }
     }
     
-       
-
+    public void compa(Nodo a1, Nodo a2){
+        //System.out.println(a1.Info);
+        //System.out.println(a2.Info);
+        boolean x = true;
+        LinkedList rec = new LinkedList();
+        LinkedList rec2 = new LinkedList();
+        int lon1=CantidadNodos(a1, rec);
+        //System.out.println(lon1);
+        int lon2=CantidadNodos2(a2, rec2);
+        //System.out.println(lon2);
+        if(lon1==lon2){
+            Inorden(a1);
+            Collections.sort(nombreA1);
+            Inorden2(a2);
+            Collections.sort(nombreA2);
+            for(int i=0; i<lon1; i++){
+                System.out.println(nombreA1.get(i));
+                System.out.println(nombreA2.get(i));
+                if(nombreA1.get(i)==nombreA2.get(i)){
+                    System.out.println("Si");
+                    x=true;
+                }else{
+                    x=false;
+                    System.out.println("No");
+                }
+            }if(x==true){
+                System.out.println("Son semejantes");
+            }else{
+                System.out.println("No son semejantes");
+            }
+        }else{
+            System.out.println("Longitud !=");
+        }
+    }
+    int prueba=0;
+    public int CantidadNodos(Nodo aux, LinkedList recorrido){
+        if(aux != null){
+            CantidadNodos(aux.Izq, recorrido);
+            recorrido.add(aux.Info);
+            CantidadNodos(aux.Dere, recorrido);
+            prueba++;
+        }
+        return prueba;
+    }
+        int prueba2=0;
+    public int CantidadNodos2(Nodo aux, LinkedList recorrido){
+        if(aux != null){
+            CantidadNodos2(aux.Izq, recorrido);
+            recorrido.add(aux.Info);
+            CantidadNodos2(aux.Dere, recorrido);
+            prueba2++;
+        }
+        return prueba2;
+    }
+    boolean sameTree(Nodo root1, Nodo root2){
+        if(root1 == null && root2 == null){ 
+            return true;
+        }
+        if(root1 == null || root2 == null){
+            return false;
+        }
+        if(root1.Info == root2.Info && sameTree(root1.Izq,root2.Izq) && sameTree(root1.Dere, root2.Dere)){
+            System.out.println("Son iguales");
+            return true;
+        }
+        System.out.println("No son iguales");
+        return false;
+    }
+    
 }
--- src/arbolessimi/ArbolesSimi.java
+++ src/arbolessimi/ArbolesSimi.java
@@ -12,16 +12,25 @@ package arbolessimi;
 public class ArbolesSimi {
     public static void main (String src[]){
         Arbol ar = new Arbol();
+        Arbol ar1 = new Arbol();
+        ar.insertar(6, 1);
+        ar.insertar(2, 2);
+        ar.insertar(3, 1);
+        ar.insertar(4, 2);
         ar.insertar(5, 1);
-        ar.insertar(3, 2);
-        ar.insertar(2, 1);
         ar.insertar(1, 2);
-        ar.insertar(4, 1);
-        ar.insertar(3, 2);
-        ar.insertar(2, 1);
-        ar.insertar(1, 2);
+
         
+        ar1.insertar(6, 1);
+        ar1.insertar(2, 2);
+        ar1.insertar(3, 1);
+        ar1.insertar(4, 2);
+        ar1.insertar(5, 1);
+        ar1.insertar(1, 2);
         
+        //ar1.sameTree(ar.raiz, ar1.raiz);
+        
+        ar1.compa(ar.raiz, ar1.raiz);
     }
     
 }
